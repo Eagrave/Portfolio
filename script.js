bed48281390ef3e16ebdaac40e02f6d833980605
Eagrave <agrave.elisar@gmail.com>
2023-09-20 13:40:14 +0800

    Completed

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleClick() {
+    const menu = document.querySelector("#hamburger-nav .menu-links");
+    const icon = document.querySelector("#hamburger-nav .hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
